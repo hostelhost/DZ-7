cae5cec538c59a2e7dc24e64bc493dd0681cb268
Илья <Hostel_2008@mail.ru>
2023-10-24 17:04:12 +0300

    Исправлены все ошибки.
    1 Попытка сдать

--- Assets/Scripts/SpawnerPlayer.cs
+++ Assets/Scripts/SpawnerPlayer.cs
@@ -1,19 +1,10 @@
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-[RequireComponent(typeof(Transform))]
 
 public class SpawnerPlayer : MonoBehaviour
 {
     [SerializeField] private Player _player;
 
     private int _maximumPlayersOnStage = 1;
-    private Transform _transform;
-
-    private void Awake()
-    {
-        _transform = GetComponent<Transform>();
-    }
 
     private void Update()
     {
